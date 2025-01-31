e101537a26091cc053cf462576585c29b39664f8
Cristian <cregueiromartinez@danielcastelao.org>
2025-01-31 14:05:29 +0100

    tablas

--- EjemploPlatypus.py
+++ EjemploPlatypus.py
@@ -3,6 +3,7 @@ from reportlab.platypus import Paragraph, Image, SimpleDocTemplate, Spacer
 from reportlab.lib.styles import getSampleStyleSheet
 from reportlab.lib.pagesizes import A4
 from reportlab.lib import colors
+from reportlab.platypus import Table
 
 hojaEstilo = getSampleStyleSheet()
 
@@ -27,6 +28,15 @@ documento.append(parrafo2)
 documento.append(Spacer(0, 20))
 imagen = Image(os.path.relpath("/home/dam/Imágenes/vinivini.jpg"), width=200, height=200)
 documento.append(imagen)
+
+
+documento.append(Spacer(0, 20))
+fila1 = ['', 'Lunes','Martes','Miercoles','Jueves','Viernes','Sábado','Domingo']
+manhana = ['Mañana', 'Estudiar','Gimnasio','Jugar resident evil','correr','ver al madrid','matarme a pajas','descansar']
+tarde = ['Tarde', 'Trabajar','Trabajar','Cagar','Trabajar','Trabajar','descanso','cagar y mear']
+Noche = ['Noche', 'descanso','Trabajar','descanso','Trabajar','salir','descanso','descanso y furbol']
+
+
 
 doc = SimpleDocTemplate("EjemploPlatypus.pdf", pagesize=A4, showBoundary=1)
 doc.build(documento)
