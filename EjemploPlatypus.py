9e6f27bca912a5b0e9b90752e2907dcf57995598
Cristian <cregueiromartinez@danielcastelao.org>
2025-02-04 10:35:34 +0100

    tablas con estilos

--- EjemploPlatypus.py
+++ EjemploPlatypus.py
@@ -29,24 +29,24 @@ documento.append(Spacer(0, 20))
 imagen = Image(os.path.relpath("/home/dam/Imágenes/vinivini.jpg"), width=150, height=150)
 documento.append(imagen)
 
-
 documento.append(Spacer(0, 20))
-#listas para la tabla
-cabeceira = ['', 'Lunes','Martes','Miercoles','Jueves','Viernes','Sábado','Domingo']
-manhana = ['Mañana', 'Estudiar','Gimnasio','Jugar','correr','ver al madrid','cagar','descansar']
-tarde = ['Tarde', 'Trabajar','Trabajar','Cagar','Trabajar','Trabajar','descanso','cagar']
-Noche = ['Noche', 'descanso','Trabajar','descanso','Trabajar','salir','descanso','furbol']
+# listas para la tabla
+cabeceira = ['', 'Lunes', 'Martes', 'Miercoles', 'Jueves', 'Viernes', 'Sábado', 'Domingo']
+manhana = ['Mañana', 'Estudiar', 'Gimnasio', 'Jugar', 'correr', 'ver al madrid', 'cagar', 'descansar']
+tarde = ['Tarde', 'Trabajar', 'Trabajar', 'Cagar', 'Trabajar', 'Trabajar', 'descanso', 'cagar']
+Noche = ['Noche', 'descanso', 'Trabajar', 'descanso', 'Trabajar', 'salir', 'descanso', 'furbol']
 
-#se crea la tabla añadiendole la lista
-tabla = Table([cabeceira,manhana,tarde,Noche])
+# se crea la tabla añadiendole la lista
+tabla = Table([cabeceira, manhana, tarde, Noche])
 documento.append(tabla)
 
-#diferentes estilos para la tabla
-tabla.setStyle([('BACKGROUND', (1,1), (-1,-1), colors.lightgrey)]) #fondo de la tabla
-tabla.setStyle([('BOX',(1,1), (-1,-1),0.5, colors.darkgrey)]) # caja que engloba la tabla
-tabla.setStyle([('INNERGRID', (1,1), (-1, -1), 0.25, colors.white)]) #lineas de la tabla
-tabla.setStyle([('TEXTCOLOR', (0,0), (0,-1), colors.red)]) #texto de la tabña izquierda
-tabla.setStyle([('TEXTCOLOR', (1,0), (-1,0), colors.pink)]) # texto de la tabla arriba
+# diferentes estilos para la tabla
+tabla.setStyle([('BACKGROUND', (1, 1), (-1, -1), colors.lightgrey)])  # fondo de la tabla
+tabla.setStyle([('BOX', (1, 1), (-1, -1), 0.5, colors.darkgrey)])  # caja que engloba la tabla
+tabla.setStyle([('INNERGRID', (1, 1), (-1, -1), 0.25, colors.white)])  # lineas de la tabla
+tabla.setStyle([('TEXTCOLOR', (0, 0), (0, -1), colors.red)])  # texto de la tabña izquierda
+tabla.setStyle([('TEXTCOLOR', (1, 0), (-1, 0), colors.pink)])  # texto de la tabla arriba
+tabla.setStyle([('ALIGN', (0, 0), (-1, -1), 'CENTER')])
 
 documento.append(Spacer(0, 20))
 
@@ -55,13 +55,18 @@ datos = [['Esquina sup', '', '02', '03', '04'],
          ['20', '21', '22', 'Esquina inf', ''],
          ['30', '31', '32', '', '']]
 
-estilo = [('GRID', (0,0), (-1,-1), 0.5, colors.grey),
-          ('BACKGROUND', (0,0), (1,1), colors.lavenderblush),
-          ('SPAN', (0,0), (1,1)),
-          ('BACKGROUND', (-2,-2), (-1,-1), colors.bisque),
-          ('SPAN', (3,2), (-1,-1))]
+estilo = [('LINEABOVE', (0, 0), (4,0), 1, colors.blue),
+          ('INNERGRID', (0, 0), (-1, -1), 0.5, colors.grey),
+          ('BACKGROUND', (0, 0), (1, 1), colors.lavenderblush),
+          ('SPAN', (0, 0), (1, 1)),
+          ('BACKGROUND', (-2, -2), (-1, -1), colors.bisque),
+          ('SPAN', (3, 2), (-1, -1)),
+          ('LINEBELOW', (0, -1), (-1, -1), 1, colors.blue),
+          ('VALIGN', (0,0), (1,1), 'MIDDLE'),
+          ('VALIGN', (-2, -2), (-1,-1), 'MIDDLE'),
+          ('ALIGN', (0,0), (-1,-1), 'CENTER')]
 
-tabla2 = Table(data= datos, style= estilo)
+tabla2 = Table(data=datos, style=estilo)
 documento.append(tabla2)
 
 doc = SimpleDocTemplate("EjemploPlatypusTabla.pdf", pagesize=A4, showBoundary=1)
